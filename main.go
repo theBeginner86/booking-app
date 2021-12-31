6288122c6cfb7f62a66a43efec6152d3f24e46f2
Pranav Singh <pranavsingh02@hotmail.com>
2021-12-31 21:27:59 +0530

    update

--- main.go
+++ main.go
@@ -12,12 +12,23 @@ func main(){
 	fmt.Printf("Welcome to %v, booking app :))\n", conferenceName)
 	fmt.Printf("Total number of tickets: %v\nRemaining Tickets: %v\n", totalTickets, remainingTickets)
 
-	username := "Jake"
-	noOfOrderedTickets := 2
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
 
-	remainingTickets = totalTickets - noOfOrderedTickets
+	fmt.Println("Enter first name: ")
+	fmt.Scan(&firstName)
 
-	fmt.Printf("User %v, booked %v tickets, remaining tickets: %v\n", username, noOfOrderedTickets, remainingTickets)
+	fmt.Println("Enter last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter email: ")
+	fmt.Scan(&email)
+
+	remainingTickets = totalTickets - userTickets
+
+	fmt.Printf("User %v %v, booked %v tickets, remaining tickets: %v\n", firstName, lastName, userTickets, remainingTickets)
 
 
 }   
