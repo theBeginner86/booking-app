2e1cf65b709e3667872138ef7abd2be1756e5564
Pranav Singh <pranavsingh02@hotmail.com>
2021-12-31 19:30:43 +0530

    starting code for app

--- main.go
+++ main.go
@@ -5,5 +5,19 @@ import (
 )
 
 func main(){
-	fmt.Println("Hello world")
-}
+	conferenceName := "Go Conf"
+	const totalTickets = 200
+	remainingTickets := totalTickets
+
+	fmt.Printf("Welcome to %v, booking app :))\n", conferenceName)
+	fmt.Printf("Total number of tickets: %v\nRemaining Tickets: %v\n", totalTickets, remainingTickets)
+
+	username := "Jake"
+	noOfOrderedTickets := 2
+
+	remainingTickets = totalTickets - noOfOrderedTickets
+
+	fmt.Printf("User %v, booked %v tickets, remaining tickets: %v\n", username, noOfOrderedTickets, remainingTickets)
+
+
+}   
